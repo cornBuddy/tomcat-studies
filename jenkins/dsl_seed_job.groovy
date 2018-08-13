b541b7cd38642a0e8b4d518ef7933ceb07588d8d
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-08-13 12:08:13 +0300

    chore(jenkins): add seed job script

---
+++ jenkins/dsl_seed_job.groovy
@@ -0,0 +1,20 @@
+STUDENT = 'hsakovich'
+
+def getJobsToExecute(indexes) {
+    indexes.collect { "MNTLAB-${STUDENT}-child${it}-build-job" }
+}
+
+job("MNTLAB-${STUDENT}-main-build-job") {
+    scm {
+        github('MNT-Lab/d323dsl', 'master')
+    }
+}
+
+getJobsToExecute((1..4)).each { jobName ->
+    job(jobName) {
+        steps {
+            shell('pwd')
+            shell('ls -la')
+        }
+    }
+}
