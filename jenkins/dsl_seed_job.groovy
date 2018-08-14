b80b413699f6bc15dd5c2327007f4e0e09da9ba0
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2018-08-14 17:18:41 +0300

    chore(jenkins): add build triggers

--- jenkins/dsl_seed_job.groovy
+++ jenkins/dsl_seed_job.groovy
@@ -1,18 +1,32 @@
+REPO = 'MNT-Lab/d323dsl'
 STUDENT = 'hsakovich'
+BRANCH_LIST = [STUDENT, 'master']
 
 def getJobsToExecute(indexes) {
     indexes.collect { "MNTLAB-${STUDENT}-child${it}-build-job" }
 }
 
+def stringifyList(list) {
+    def decoratedItems = list.collect { "'${it}'" }
+    "[${decoratedItems.join(', ')}]"
+}
+
 job("MNTLAB-${STUDENT}-main-build-job") {
     scm {
-        github('MNT-Lab/d323dsl', 'master')
+        github(REPO, 'master')
     }
     parameters {
-        gitParam('BRANCH_NAME') {
-            type('BRANCH')
+        activeChoiceParam('BUILDS_TRIGGER') {
+            description('Select the jobs')
+            choiceType('CHECKBOX')
+            filterable(false)
+            groovyScript {
+                script(stringifyList(getJobsToExecute((1..4))))
+                // script('["choice1", "choice2"]')
+                fallbackScript('"fallback choice"')
+            }
         }
-        choiceParam('BUILDS_TRIGGER', getJobsToExecute((1..4)))
+        choiceParam('BRANCH_NAME', BRANCH_LIST, 'Select target branch')
     }
 }
 
